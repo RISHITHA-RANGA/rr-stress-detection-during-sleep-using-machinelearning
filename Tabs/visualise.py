67a04714f455988390a7558622de62e5c5c59100
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-01-06 15:28:25 +0530

    Update visualise.py

--- Tabs/visualise.py
+++ Tabs/visualise.py
@@ -4,7 +4,7 @@
 import warnings
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.metrics import plot_confusion_matrix
+'''from sklearn.metrics import plot_confusion_matrix'''
 from sklearn import tree
 import streamlit as st
 
@@ -68,10 +68,5 @@ def app(df, X, y):
 
     
 
-    if st.checkbox("Plot confusion matrix"):
-        model, score = train_model(X, y)
-        plt.figure(figsize = (10, 6))
-        plot_confusion_matrix(model, X, y, values_format='d')
-        st.pyplot()
-
+    
     
