38c1ba78d2398275472ff35607ccea2d58b153c7
RISHITHA-RANGA <123916357+RISHITHA-RANGA@users.noreply.github.com>
2023-09-19 22:54:59 +0530

    Update home.py

--- Tabs/home.py
+++ Tabs/home.py
@@ -1,20 +1,9 @@
-"""This modules contains data about home page"""
-
-# Import necessary modules
 import streamlit as st
-
 def app():
-    """This function create the home page"""
-    
-    # Add title to the home page
     st.title("Stress Level Detector")
-
-    # Add image to the home page
-    st.image("./images/home.png")
-
-    # Add brief describtion of your web app
+    st.image("./images/home stress image.jpg")
     st.markdown(
     """<p style="font-size:20px;">
-            Considering today’s lifestyle, people just sleep forgetting the benefits sleep provides to the human body. Smart-Yoga Pillow (SaYoPillow) is proposed to help in understanding the relationship between stress and sleep and to fully materialize the idea of “Smart-Sleeping” by proposing an edge device. An edge processor with a model analyzing the physiological changes that occur during sleep along with the sleeping habits is proposed. Based on these changes during sleep, stress prediction for the following day is proposed. The secure transfer of the analyzed stress data along with the average physiological changes to the IoT cloud for storage is implemented. A secure transfer of any data from the cloud to any third-party applications is also proposed. A user interface is provided allowing the user to control the data accessibility and visibility. SaYoPillow is novel, with security features as well as consideration of sleeping habits for stress reduction, with an accuracy of up to 96%.
+              The SaYoPillow is designed to address the importance of sleep in our busy lives and promote the idea of "Smart-Sleeping." It incorporates an edge processor with a sleep analysis model to monitor physiological changes and sleep habits. Using this data, it predicts stress levels for the next day and securely stores it in the IoT cloud. Data can also be securely transferred to third-party apps. Users have control over data access through a user-friendly interface. This innovative pillow prioritizes sleep patterns to reduce stress, boasting an accuracy rate of up to 96%.
         </p>
     """, unsafe_allow_html=True)
